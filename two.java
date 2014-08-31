8bd34fc378b9d3e40710c4fed201a7585ec138c6
Jenny Liang <cl2540@nyu.edu>
2014-08-31 01:38:17 +0100

    Problem 2 solutions

--- two.java
+++ two.java
@@ -2,6 +2,45 @@ public class two
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Hellow World adsfasdfasdfmore!!!");
+		double [] num; 
+		num = new double[100]; 
+
+		num[0] = 1; 
+		num [1] = 2; 
+
+		for (int x = 2; x < 100; x++) // intialize array position counter 
+		{
+			num[x] = (num[x-1]) + (num[x-2]); // arithmetic/definition of fibonacci sequence
+			
+		}
+
+		for (int x = 0; x< 100; x++) // check if fibonacci sequence is created
+		{
+			//System.out.println(x + ": "+  num[x]);
+		
+		}	
+
+
+	
+		int sum = 0; // initiate sum
+
+
+		for (int z = 0;  z< 100; z++) // sum up even numbers
+		{
+			if (num[z] < 4000000) // if number in position is less than 4 million
+			{
+				if (num[z] % 2 == 0)  // check if even
+				{			
+					sum += num[z];
+					
+				}
+				else
+				{
+					continue;
+				}
+			}
+
+		}System.out.println(sum);
+		
 	}
 }
